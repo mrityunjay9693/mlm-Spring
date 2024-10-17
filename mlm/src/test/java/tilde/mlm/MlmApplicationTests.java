adc8f1ad61824ed1113730cf4ad2f5ab5c85b62e
mrityunjay9693 <mkaykumar123@gmail.com>
2024-10-17 18:42:00 +0530

    project initialized and set up

---
+++ mlm/src/test/java/tilde/mlm/MlmApplicationTests.java
@@ -0,0 +1,13 @@
+package tilde.mlm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
