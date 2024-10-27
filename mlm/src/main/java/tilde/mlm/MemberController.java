43eb479616c97364e1b9a3a7c125e09f52c2d635
mrityunjay9693 <mkaykumar123@gmail.com>
2024-10-27 12:33:33 +0530

    controller

--- mlm/src/main/java/tilde/mlm/MemberController.java
+++ mlm/src/main/java/tilde/mlm/MemberController.java
@@ -1,8 +1,31 @@
 package tilde.mlm;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @Controller
 public class MemberController {
+    @GetMapping("/member")
+    public String member() {
+        return "member\\member";
+    }
     
+    @GetMapping("member/create")
+    public String create() {
+        //return new String(); //returning a view
+        return "member//create";
+    }
+
+    @PostMapping("/member/store")
+    public String store(@RequestBody String entity) {
+        //TODO: process POST request
+        
+        //return entity;
+        return "member\\member";
+    }
 }
